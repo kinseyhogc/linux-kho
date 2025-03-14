06e530fbd2bf0de0319bfb2b28b92e67e4202f96
Kinsey Ho <kinseyho@google.com>
2025-03-13 22:26:21 -0700

    kexec: pass kexec telemetry to new kernel via KHO

--- kernel/kexec_telemetry.c
+++ kernel/kexec_telemetry.c
@@ -5,7 +5,9 @@
 #include <linux/sched/clock.h>
 #include <linux/limits.h>
 #include <linux/string.h>
+#include <linux/libfdt.h>
 #include <linux/kexec.h>
+#include <linux/kexec_handover.h>
 #include <linux/sort.h>
 
 struct timestamp_info {
@@ -22,6 +24,8 @@ static atomic_t ts_index;
 static atomic_t event_index;
 
 int kexec_timestamp(char *event_name);
+
+struct kho_node telemetry_node = KHO_NODE_INIT;
 
 static int add_timestamp_to_telemetry_array(struct timestamp_info ts_info, char *event_name)
 {
@@ -103,8 +107,8 @@ static int kexec_telemetry_show(struct seq_file *m, void *v)
 
 	seq_printf(m, "%-25s%s\n", "Event", "Timestamp");
 	seq_printf(m, "-------------------------------------\n");
-
 	// RCU read lock
+	//
 	kexec_telemetry_sort();
 
 	for (; i < index ; i++) {
@@ -157,10 +161,132 @@ static const struct file_operations kexec_telemetry_reset_fops = {
 	.write = kexec_telemetry_reset,
 };
 
+static int kho_telemetry_notifier(struct notifier_block *self, unsigned long cmd,
+				  void *private)
+{
+	char *event_name_buf;
+	unsigned long *ts_buf;
+	bool *src_buf; //K:0, U:1, also TODO: use bitmap to save space
+	size_t names_total_size = 0;
+	size_t event_count;
+	int ts_iter = 0;
+	int name_offset = 0;
+
+	kexec_telemetry_sort();
+	event_count = atomic_read(&ts_index);
+
+	/* Calculate size of temp buffer for event names and timestamp */
+	for (; ts_iter < event_count; ts_iter++) {
+		names_total_size += strlen(ts_array[ts_iter].name) + 1;
+	}
+
+	event_name_buf = kzalloc(names_total_size, GFP_KERNEL);
+	ts_buf = kzalloc((sizeof(unsigned long)*event_count), GFP_KERNEL);
+	src_buf = kzalloc((sizeof(bool)*event_count), GFP_KERNEL);
+
+	if (!event_name_buf || !ts_buf || !src_buf)
+		return NOTIFY_BAD;
+
+	/* Serialize names and timestamp */
+	for (ts_iter = 0; ts_iter < event_count; ts_iter++) {
+		struct timestamp_info t = ts_array[ts_iter];
+		size_t name_len = strlen(t.name) + 1;
+		memcpy(event_name_buf + name_offset, t.name, name_len);
+		ts_buf[ts_iter] = t.ts;
+		src_buf[ts_iter] = t.src_user;
+
+		name_offset += name_len;
+	}
+
+	kho_add_node(NULL, "kexec_telemetry", &telemetry_node);
+	kho_add_prop(&telemetry_node, "event_name", event_name_buf, names_total_size);
+	kho_add_prop(&telemetry_node, "timestamp", ts_buf, sizeof(unsigned long)*event_count);
+	kho_add_prop(&telemetry_node, "src", src_buf, event_count);
+
+	return NOTIFY_DONE;
+}
+
+/* Re-build telemetry file */
+static int __restore_telemetry_from_kho(const char *name, size_t name_len, unsigned long ts, int src_user)
+{
+	struct timestamp_info ts_info;
+	char name_buf[NAME_MAX];
+
+	memcpy(name_buf, name, name_len);
+
+	ts_info.ts = ts;
+	ts_info.src_user = src_user;
+
+	add_timestamp_to_telemetry_array(ts_info, name_buf);
+
+	return 0;
+}
+
+static int restore_telemetry_from_kho(void)
+{
+	const void *fdt = kho_get_fdt();
+	int offset, err, i = 0;
+	int names_size = 0;
+	int ts_buf_size = 0;
+	int src_buf_size = 0;
+
+	const char *names;
+	const unsigned long *ts_buf;
+	const bool *src_buf;
+
+	const char *event_name;
+
+	if (!fdt)
+		return 0;
+
+	offset = fdt_path_offset(fdt, "/kexec_telemetry");
+	if (offset < 0) {
+		pr_err("cannot find /timestamps in KHO dt\n");
+		return -ENOENT;
+	}
+
+	names = fdt_getprop(fdt, offset, "event_name", &names_size);
+	if (!names) {
+		pr_err("Looking for /kexec_telemetry.event_name, found p=%p\n", names);
+		return -ENOENT;
+	}
+
+	ts_buf = fdt_getprop(fdt, offset, "timestamp", &ts_buf_size);
+	if (!ts_buf) {
+		pr_err("Looking for /kexec_telemetry.timestamp, found p=%p\n", ts_buf);
+		return -ENOENT;
+	}
+
+	src_buf = fdt_getprop(fdt, offset, "src", &src_buf_size);
+	if (!ts_buf) {
+		pr_err("Looking for /kexec_telemetry.src, found p=%p\n", src_buf);
+		return -ENOENT;
+	}
+	event_name = names;
+
+	printk("%s, restoring\n", __func__);
+	for (; i < src_buf_size; i += 1) {
+		size_t name_len = strlen(event_name) + 1;
+
+		err = __restore_telemetry_from_kho(event_name, name_len, ts_buf[i], src_buf[i]);
+		if (err)
+			return err;
+		event_name += name_len;
+	}
+
+	return err;
+
+}
+
+static struct notifier_block kho_telemetry_nb = {
+	.notifier_call = kho_telemetry_notifier,
+};
+
 static int __init kexec_telemetry_init(void)
 {
-	kexec_telemetry_debugfs_dir = debugfs_create_dir("kexec", NULL);
+	int err;
 
+	kexec_telemetry_debugfs_dir = debugfs_create_dir("kexec", NULL);
 	debugfs_create_file("timestamps", 0644, kexec_telemetry_debugfs_dir, NULL, &kexec_telemetry_fops);
 	debugfs_create_file("timestamps_reset", 0200, kexec_telemetry_debugfs_dir, NULL, &kexec_telemetry_reset_fops);
 
@@ -175,6 +301,10 @@ static int __init kexec_telemetry_init(void)
 	atomic_set(&ts_index, 0);
 	atomic_set(&event_index, 0);
 
-	return 0;
+	err = restore_telemetry_from_kho();
+
+	register_kho_notifier(&kho_telemetry_nb);
+
+	return err;
 }
 subsys_initcall(kexec_telemetry_init);
